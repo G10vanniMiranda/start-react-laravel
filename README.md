475c4add04fd059bcc048806597224f15b3cef0a
Giovanni Miranda <giovannimiranda09@gmail.com>
2024-11-18 20:29:37 -0400

    first commit

---
+++ README.md
@@ -0,0 +1,9 @@
+#### Pricipais pastas
+    * app
+        * Http -->   Controllers
+        * Models --> Representa o modelo da tabela
+    * database
+        * Migrations --> script de crição das tabelas
+        * Seeders --> informações iniciais que carregamos na tabela
+    * resources --> views html, css, javascript
+    * routes --> rotas de configuração para aplicação, login, tela de cadastro, produtos
---
+++ database/seeders/UserSeeder.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use App\Models\User;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        User::firstOrCreate(
+            [ 'email' => 'admin@gmail.com' ],
+            [ 'name' => 'Sebastião', 'email' => 'admin@gmail.com',
+            'password' => '1234admin']
+        );
+        User::firstOrCreate(
+            [ 'email' => 'admin1@gmail.com' ],
+            [ 'name' => 'Edson', 'email' => 'admin1@gmail.com',
+            'password' => '1234admin']
+        );
+    }
+}
